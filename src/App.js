89c2df6cd8d442010393119881b8a8bb04173873
amritasookram101 <amritasookram101@yahoo.com>
2021-01-16 18:31:17 -0500

    hjjf

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import "./App.css";
 import Header from "./components/Header";
-import FirstSection from "./components/FirstSection";
+import SimpleBottomNavigation from "./components/SimpleBottomNavigation";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <FirstSection></FirstSection>
+      <SimpleBottomNavigation></SimpleBottomNavigation>
     </div>
   );
 }
